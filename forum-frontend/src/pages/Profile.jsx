23d41ef659c0371206cb7bb09b365807ef3fbc2c
Batuhan Keskin <batuhankeskn0@gmail.com>
2026-02-18 15:13:35 +0300

    Login button update 5

--- forum-frontend/src/pages/Profile.jsx
+++ forum-frontend/src/pages/Profile.jsx
@@ -141,13 +141,6 @@ const Profile = ({ isOpen, onClose, onLogout }) => {
                     </button>
                 </div>
 
-                <button class="cta">
-                    <span>Hover me</span>
-                    <svg width="15px" height="10px" viewBox="0 0 13 10">
-                        <path d="M1,5 L11,5"></path>
-                        <polyline points="8 1 12 5 8 9"></polyline>
-                    </svg>
-                </button>
 
             </div>
         </>
